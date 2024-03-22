25f853a9f86250233702df609aba56faf805f398
Akash G U <akashgu10@gmail.com>
2024-03-22 17:40:26 +0530

    Adding data base to py file

--- README.md
+++ README.md
@@ -3,10 +3,9 @@ analyzing business needs and in utilizing Python and SQL to provide solutions.
 
 ##Steps Followed are
 
-1. "dummydata.py" has dummy data which is taken for an example for getting out the solutions
-2. "etl.py" has python scripts which is to get the required results as per given objectives
-3. "etl_with_dummy_data.py" is contains both dummydata and python scripts, can run in jupiter lab
-4. "etl_SQl.sql" same as py file
+1. "etl.py" connecting to sqlite3 by downloading and uploading to google colab has python scripts which is to get the required results as per given objectives
+2. "eastvantage.ipynb is contains python scripts, can run in jupiter lab or colab
+3. "etl_SQl.sql" same as py file
 
 ##Package Required 
 
--- etl.py
+++ etl.py
@@ -1,33 +1,36 @@
 import sqlite3
-import csv
-import pandas as pd
 
-# Connect to SQLite database
-conn = sqlite3.connect('sales_database.db')
+# Connect to SQLite database (creates new database if not exists)
+conn = sqlite3.connect('/content/Data Engineer_ETL Assignment.db')
 cursor = conn.cursor()
 
-# Execute SQL query to get total quantities of each item bought per customer aged 18-35
-cursor.execute('''
-    SELECT O.Quantity,C.CustomerID,C.Age,I.ItemID,I.Name
-    FROM Orders as O
-    LEFT JOIN Sales S ON O.SaleID=S.SaleID
-    LEFT JOIN Items I ON O.ItemID=I.ItemID
-    LEFT JOIN Customers C ON C.CustomerID=S.CustomerID
 
-''')
+cursor.execute(
+    '''
+    SELECT O.quantity,C.customer_id,C.age,I.item_id,I.item_name
+    FROM orders as O
+    LEFT JOIN sales S ON O.sales_id=S.sales_id
+    LEFT JOIN items I ON O.item_id=I.item_id
+    LEFT JOIN customers C ON C.customer_id=S.customer_id
 
-# Fetch all results
+    '''
+)
 results = cursor.fetchall()
+
 col_df=pd.DataFrame(results,columns=["Quantity","Customer","Age","Item","Name"])
 # Filter customers aged 18 to 35
 filtered_sales_df = col_df[(col_df['Age'] >= 18) & (col_df['Age'] <= 35)]
+# print(filtered_sales_df)
 # Perform aggregation
 aggregated_df = filtered_sales_df.groupby(['Customer','Age','Name','Item'])['Quantity'].sum().reset_index()
 aggregated_df.rename(columns={'Quantity':'total_quantity'},inplace=True)
 # Filter out items with total quantity = 0
 filtered_aggregated_df=aggregated_df[aggregated_df['total_quantity']>0]
-# # Write results to CSV file with semicolon delimiter
+# Write results to CSV file with semicolon delimiter
 filtered_aggregated_df.to_csv('customer_items_aggregated.csv', sep=';', index=False)
-# Close connection
-conn.close()
 print("Query results stored in customer_items.csv successfully.")
+# # Commit changes and close connection
+conn.commit()
+conn.close()
+
+# print("Database created successfully.")
